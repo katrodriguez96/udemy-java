6cccb58c36494a175922dcce43e7abff388b7ee4
Kathryn Rodriguez <kathryn.n.rodriguez@gmail.com>
2022-06-23 13:15:47 -0500

    char and boolean

---
+++ src/CharAndBoolean.java
@@ -0,0 +1,11 @@
+public class CharAndBoolean {
+    public static void main(String[] args) {
+        char myChar = 'D';
+        char unicodeChar = '\u0044';
+        System.out.println("myChar = " + myChar);
+        System.out.println("unicodeChar = " + unicodeChar);
+
+        boolean myTrueValue = true;
+        boolean myFalseValue = false;
+    }
+}
