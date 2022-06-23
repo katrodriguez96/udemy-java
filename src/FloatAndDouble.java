e4dfb65289337b197756de17ea33698b2342ccf1
Kathryn Rodriguez <kathryn.n.rodriguez@gmail.com>
2022-06-23 12:51:59 -0500

    float and double primitive types

---
+++ src/FloatAndDouble.java
@@ -0,0 +1,17 @@
+public class FloatAndDouble {
+    public static void main(String[] args) {
+        float minFloatValue = Float.MIN_VALUE;
+        float maxFloatValue = Float.MAX_VALUE;
+        System.out.println("Minimum float value: " + minFloatValue);
+        System.out.println("Maximum float value: " + maxFloatValue);
+
+        double minDoubleValue = Double.MIN_VALUE;
+        double maxDoubleValue = Double.MAX_VALUE;
+        System.out.println("Minimum double value: " + minDoubleValue);
+        System.out.println("Maximum double value: " + maxDoubleValue);
+
+        int intValue = 5;
+        float floatValue = 5.25f;
+        double doubleValue = 5.25d;
+    }
+}
