2665214f3df13d26717575045bc01950e148d3ca
Kathryn Rodriguez <kathryn.n.rodriguez@gmail.com>
2022-06-28 15:04:07 -0500

    abbreviating operators

--- src/Operators.java
+++ src/Operators.java
@@ -16,5 +16,28 @@ public class Operators {
 
         result = result % 3; // the remainder of (4 % 3) = 1
         System.out.println("result = " + result);
+
+        // result = result + 1;
+        result++; // 1 + 1 = 2
+        System.out.println("result = " + result);
+
+        result--; // 2 - 1 = 1
+        System.out.println("result = " + result);
+
+        // result = result + 2;
+        result += 2; // 1 + 2 = 3
+        System.out.println("result = " + result);
+
+        // result = result * 10;
+        result *= 10; // 3 * 10 = 30
+        System.out.println("result = " + result);
+
+        // result = result / 3;
+        result /= 3; // 30 / 3 = 10
+        System.out.println("result = " + result);
+
+        // result = result - 2;
+        result -= 2; // 10 - 2 = 8
+        System.out.println("result = " + result);
     }
 }
