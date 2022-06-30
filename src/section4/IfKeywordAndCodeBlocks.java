55d3ecbfe072dc03f9bbcc90c80e2c6075a1b8a8
Kathryn Rodriguez <kathryn.n.rodriguez@gmail.com>
2022-06-30 16:58:37 -0500

    methods

--- src/section4/IfKeywordAndCodeBlocks.java
+++ src/section4/IfKeywordAndCodeBlocks.java
@@ -7,18 +7,7 @@ public class IfKeywordAndCodeBlocks {
         int levelCompleted = 5;
         int bonus = 100;
 
-//        if (score < 5000 && score > 1000) {
-//            System.out.println("Your score was less than 5000 but greater than 1000");
-//        } else if (score < 1000) {
-//            System.out.println("Your score was less than 1000");
-//        } else {
-//            System.out.println("Got here");
-//        }
-
-        if (gameOver) {
-            int finalScore = score + (levelCompleted * bonus);
-            System.out.println("Your final score was " + finalScore);
-        }
+        calculateScore(gameOver, score, levelCompleted, bonus);
 
         // challenge
         score = 10000;
@@ -27,6 +16,17 @@ public class IfKeywordAndCodeBlocks {
         if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
             System.out.println("Your final score was " + finalScore);
+        }
+    }
+
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+        if (gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore += 1000;
+            System.out.println("Your final score was " + finalScore);
+            return finalScore;
+        } else {
+            return -1;
         }
     }
 }
